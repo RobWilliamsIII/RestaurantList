ca018a1b1f50a073549939ceaabbdcf11b34fa25
robwilliams <robwilliams@localhost>
2023-12-12 11:35:14 -0700

    Added pseudocode file and updated RestuarantList with better verbiage to meet assignment prompt

---
+++ RestuarantList/pesuedocode.java
@@ -0,0 +1,18 @@
+// Pseudocode for Restaurant List Program
+
+BEGIN
+
+ //Delcare Variables
+
+String restaurantName = "Tangerine";
+String restaurantAddress = "2777 Iris Ave";
+String restaurantCityStateZip = "Boulder, CO, 80304";
+
+
+// Display Variables
+
+display "Restaurant Name: " + restaurantName);
+display "Restaurant Address: " + restaurantAddress);
+display "Restaurant City, State and Zipcode: " + restaurantCityStateZip);
+
+END
--- RestuarantList/src/com/restaurantlist/model/Restaurant.java
+++ RestuarantList/src/com/restaurantlist/model/Restaurant.java
@@ -4,11 +4,11 @@ public class Restaurant {
 
 	public static void main(String[] args) {
 		String restaurantName = "Tangerine";
-		String restaurantAddress = "2777 Iris Ave";
+		String restaurantAddress = "2777 Iris Ave.";
 		String restaurantCityStateZip = "Boulder, CO, 80304";
-		System.out.println(restaurantName);
-		System.out.println(restaurantAddress);
-		System.out.println(restaurantCityStateZip);
+		System.out.println("Restaurant Name: " + restaurantName);
+		System.out.println("Restaurant Address: " + restaurantAddress);
+		System.out.println("Restaurant City, State and Zipcode: " + restaurantCityStateZip);
 
 	}
 
