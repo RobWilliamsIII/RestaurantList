3e46867aa2e8854ea492f356dc7acd6c48173747
robwilliams <robwilliams@localhost>
2023-12-12 11:22:13 -0700

    updated output to match assignment requirements

--- RestuarantList/src/com/restaurantlist/model/Restaurant.java
+++ RestuarantList/src/com/restaurantlist/model/Restaurant.java
@@ -3,9 +3,9 @@ package com.restaurantlist.model;
 public class Restaurant {
 
 	public static void main(String[] args) {
-		System.out.println("Tangerine");
-		System.out.println("2777 Iris Ave");
-		System.out.println("Boulder, CO, 80304");
+		System.out.println("Restaurant name: Tangerine");
+		System.out.println("Business address: 2777 Iris Ave");
+		System.out.println("City, State, Zip Coode: Boulder, CO, 80304");
 
 	}
 
