add35b002188c7367312435cc1f9727289e17002
robwilliams <robwilliams@localhost>
2023-12-12 10:58:56 -0700

    initial commit.

---
+++ RestuarantList/src/com/restaurantlist/model/Restaurant.java
@@ -0,0 +1,12 @@
+package com.restaurantlist.model;
+
+public class Restaurant {
+
+	public static void main(String[] args) {
+		System.out.println("Tangerine");
+		System.out.println("2777 Iris Ave");
+		System.out.println("Boulder, CO, 80304");
+
+	}
+
+}
